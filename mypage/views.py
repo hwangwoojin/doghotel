0efe23bc40856c3886249eb67961cbbc2411cee5
hwangwoojin <hwangwoojin@hwangwoojinui-MacBookPro.local>
2020-06-11 21:28:10 +0900

    add mypage

--- mypage/views.py
+++ mypage/views.py
@@ -1,9 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import user
 
 # Create your views here.
 
 def mypage(request):
-    return render(request, 'mypage/mypage.html')
+    # get userinfo from queryset
+    userinfo = user.objects.get(id='hwangwoojin')
+    # POST
+    if request.method == 'POST':
+        # update database
+        userinfo.name = request.POST['name']
+        userinfo.email = request.POST['email']
+        userinfo.phone = request.POST['phone']
+        userinfo.save()
+        return redirect('home')
+    # GET
+    else:
+        userinfo = user.objects.get(id='hwangwoojin')
+        return render(request, 'mypage/mypage.html', {'userinfo': userinfo})
 
 def mydog(request):
     return render(request, 'mypage/mydog.html')
